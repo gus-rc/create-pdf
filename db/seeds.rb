c646e33500ccee379b57c52c1fce63edb0436a1f
gus-rc <tavo.reyes.c@gmail.com>
2022-03-10 00:24:10 -0600

    se actualiza codigo

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(title: 'Agua', pricing: 10)
+Product.create(title: 'Pan', pricing: 12)
+Product.create(title: 'Leche', pricing: 11)
+Product.create(title: 'Cereal', pricing: 19)
+Product.create(title: 'Dulces', pricing: 13)
+Product.create(title: 'Ropa', pricing: 14)
+Product.create(title: 'Azucar', pricing: 12)
+Product.create(title: 'Sal', pricing: 30)
+Product.create(title: 'Verdura', pricing: 35)
+Product.create(title: 'Limpieza', pricing: 25)
